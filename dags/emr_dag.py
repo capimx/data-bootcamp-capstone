93e678223207058b068ac544e87824512e83ac09
Mauricio Caballero <mauricio.caballero@wizeline.com>
2022-03-11 11:47:53 -0600

    Add v1 of movie reviews script + dag First version of moview reviews job created in pyspark, plus an initial separate dag

---
+++ dags/emr_dag.py
@@ -0,0 +1,64 @@
+"""
+This is an example dag for a AWS EMR Pipeline with auto steps.
+"""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator
+from airflow.providers.amazon.aws.sensors.emr import EmrJobFlowSensor
+
+# [START howto_operator_emr_automatic_steps_config]
+SPARK_STEPS = [
+    {
+        'Name': 'calculate_pi',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': ['s3://wz-de-academy-mau-scripts/movie_reviews_job.py', 'SparkPi', '10'],
+        },
+    }
+]
+
+JOB_FLOW_OVERRIDES = {
+    'Name': 'PiCalc',
+    'ReleaseLabel': 'emr-5.29.0',
+    'Applications': [{'Name': 'Spark'}],
+    'Instances': {
+        'InstanceGroups': [
+            {
+                'Name': 'Primary node',
+                'Market': 'SPOT',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 't3.large',
+                'InstanceCount': 1,
+            }
+        ],
+        'KeepJobFlowAliveWhenNoSteps': False,
+        'TerminationProtected': False,
+    },
+    'Steps': SPARK_STEPS,
+    'JobFlowRole': 'EMR_EC2_DefaultRole',
+    'ServiceRole': 'EMR_DefaultRole',
+}
+# [END howto_operator_emr_automatic_steps_config]
+
+with DAG(
+    dag_id='emr_job_flow_automatic_steps_dag',
+    dagrun_timeout=timedelta(hours=2),
+    start_date=datetime(2021, 1, 1),
+    schedule_interval='0 3 * * *',
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    # [START howto_operator_emr_automatic_steps_tasks]
+    job_flow_creator = EmrCreateJobFlowOperator(
+        task_id='create_job_flow',
+        job_flow_overrides=JOB_FLOW_OVERRIDES,
+    )
+
+    job_sensor = EmrJobFlowSensor(task_id='check_job_flow', job_flow_id=job_flow_creator.output)
+    # [END howto_operator_emr_automatic_steps_tasks]
+
+    # Task dependency created via `XComArgs`:
+    #   job_flow_creator >> job_sensor
---
+++ spark/movie_reviews_job.py
@@ -0,0 +1,66 @@
+#import pyspark
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover
+from pyspark.sql.functions import array_contains
+#from pyspark.sql import 
+#from pyspark import SparkContext, SparkConf
+#import sys
+from pyspark.sql import SparkSession
+
+
+#from pyspark.context import SparkContext
+
+
+if __name__ == '__main__':
+
+    spark = SparkSession.builder\
+            .master("local")\
+            .appName("MovieReviews")\
+            .getOrCreate()
+
+    dfReviews = spark.read.format('csv').options(header='true', inferSchema='true').load("s3://wz-de-academy-mau-raw-data/movie_review.csv")
+
+    print(dfReviews.printSchema())
+
+    tokenizer = Tokenizer(inputCol="review_str", outputCol="review_tokens")
+    remover = StopWordsRemover(inputCol="review_tokens", outputCol="filtered_review_tokens")
+    #remover.setInputCol("")
+    #remover.setOutputCol("")
+    remover.loadDefaultStopWords('english')
+    dfReviews = tokenizer.transform(dfReviews)
+    dfReviews = remover.transform(dfReviews)
+    dfReviews = dfReviews.withColumn("positive_review", array_contains(dfReviews.filtered_review_tokens,"good").cast('integer') )
+    columns_to_write = ["cid","positive_review"]
+    #df.select(columns_to_write).write.csv("s3a://deb-silver/movie_positive_reviews.csv")
+
+    dynamic_f_cleaned = dfReviews.select(columns_to_write)
+
+    #datasink2 = spark.write_dynamic_frame.from_options(frame = dynamic_f_cleaned, connection_type = "s3", connection_options = {"path": "s3://deb-silver//positive_reviews"}, format = "parquet")
+    dynamic_f_cleaned.write.mode("overwrite").parquet("s3://wz-de-academy-mau-stage-data")
+    spark.stop()
+
+""" dynamic_f = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options = {"paths": ["s3://deb-bronze"]}, format = "csv", format_options={
+        "withHeader": True,
+        "separator": ","
+    })
+
+#spark.read.option("header",True).csv("s3a://deb-bronze/movie_review.csv")
+#df.printSchema()
+df = dynamic_f.toDF()
+
+tokenizer = Tokenizer(inputCol="review_str", outputCol="review_tokens")
+remover = StopWordsRemover(inputCol="review_tokens", outputCol="filtered_review_tokens")
+#remover.setInputCol("")
+#remover.setOutputCol("")
+remover.loadDefaultStopWords('english')
+df = tokenizer.transform(df)
+df = remover.transform(df)
+
+df = df.withColumn("positive_review", pyspark.sql.functions.array_contains(df.filtered_review_tokens,"good").cast('integer') )
+columns_to_write = ["cid","positive_review"]
+#df.select(columns_to_write).write.csv("s3a://deb-silver/movie_positive_reviews.csv")
+
+dynamic_f_cleaned = DynamicFrame.fromDF(df.select(columns_to_write), glueContext,"positive_reviews")
+
+datasink2 = glueContext.write_dynamic_frame.from_options(frame = dynamic_f_cleaned, connection_type = "s3", connection_options = {"path": "s3://deb-silver//positive_reviews"}, format = "parquet")
+job.commit()
+#df.select(columns_to_write).write.parquet("c:/test.parquet",mode="overwrite") """
